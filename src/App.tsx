48fb36b957967a2d3166b8d4bab2b3984186c1d0
Robert Nochez <robertnochez@berkeley.edu>
2024-06-22 13:30:57 -0700

    changed to typescript and added battlemap

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Battlemap from './components/Battlemap/Battlemap';
+
+function App() {
+  return (
+    <div id="app">
+      <Battlemap/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Battlemap/Battlemap.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Battlemap.css'
+
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8]
+const horizontalAxis = [1, 2, 3, 4, 5, 6, 7, 8]
+
+export default function Battlemap() {
+    let map = [];
+
+    for (let i = verticalAxis.length-1; i >= 0; i--) {
+        for (let j = 0; j<horizontalAxis.length; j++) {
+            const num = j + i + 2;
+
+            if (num % 2 === 0) {
+                map.push(
+                    <div className="title black-tile"></div>
+                );
+            } else {
+                map.push(
+                    <div className="title white-tile"></div>
+                );
+            }
+        }
+    }
+    return <div id= "battlemap">{map}</div>
+}
