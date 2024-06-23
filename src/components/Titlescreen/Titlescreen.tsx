b75e146ee180c7480a96054bc03c4e226e7e315e
Isabella Alpert <isabella.alps@berkeley.edu>
2024-06-22 19:05:13 -0700

    ui updated

--- src/components/Titlescreen/Titlescreen.tsx
+++ src/components/Titlescreen/Titlescreen.tsx
@@ -8,11 +8,23 @@ const handleArrowClick = () => {
     }
   };
 
+  const handleHomeClick = () => {
+    window.scrollTo({
+        top: 0, 
+        behavior: "smooth" 
+      });
+  };
+
 const Page = () => {
 
     return (
         <div>
-            <div className="sticky-bar-top"></div>
+            <div className="sticky-bar-top">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" style={{ height: '30px', width: '30px'}} onClick={handleHomeClick}>
+                    <path d="M3 9l9-7 9 7v11H3V9z" /> 
+                    <path d="M9 21V12h6v9" />
+                </svg>
+            </div>
             <section id="section-home">
             <div className="image-container">
                 <img src="/assets/dt-logo-grey.png" alt="DT Logo" style={{ height: '300px', width: '300px' }}/>
@@ -34,9 +46,31 @@ const Page = () => {
                 </div>
             </div>
             </section>
+
             <section id="section-what-we-do" className="section section-what-we-do">
                 <div className="vignette"></div>
-                <h1> test </h1>
+                <div className="right-column"></div>
+                <div className="left-column">
+                    <h1>Test new characters easily!</h1>
+                    <div className="circle-container">
+                        <div className="circle-text">
+                            <div className="circle">1</div>
+                            <h2>Choose your class</h2>
+                        </div>
+                    </div>
+                    <div className="circle-container">
+                        <div className="circle-text">
+                            <div className="circle">2</div>
+                            <h2>Customize your stats</h2>
+                        </div>
+                    </div>
+                    <div className="circle-container">
+                        <div className="circle-text">
+                            <div className="circle">3</div>
+                            <h2>Simulate battles against enemies</h2>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     );
